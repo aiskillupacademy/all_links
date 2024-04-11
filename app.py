bb52c91aa1cc15e00c5097193302741cc689cfa4
aiskillupacademy <160478752+aiskillupacademy@users.noreply.github.com>
2024-04-11 12:53:13 +0000

    Added brand voice link

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ st.title("Rava.AI")
 url1 = "https://dynamic-prompt-generator.streamlit.app/"
 url2 = "https://static-workflows.streamlit.app/"
 url3 = "https://strategy-module.streamlit.app/"
+url4 = "https://brand-voice-promptgen.streamlit.app/"
 
 # Create hyperlinks using markdown
 st.markdown(f"[Dynamic Prompt Generator🚀]({url1})")
 st.markdown(f"[Static Workflows🔄]({url2})")
 st.markdown(f"[Strategy Module♞]({url3})")
+st.markdown(f"[Brand Voice Prompt Generator🎙️]({url4})")
