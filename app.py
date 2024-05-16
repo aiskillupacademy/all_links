f57dc1dda58da256aeb72c5121e2561ee24d8a33
aiskillupacademy <160478752+aiskillupacademy@users.noreply.github.com>
2024-05-16 06:17:29 +0000

    Added Subham's chatbot

--- app.py
+++ app.py
@@ -13,11 +13,16 @@ url3 = "https://strategy-module.streamlit.app/"
 url4 = "https://brand-voice-promptgen.streamlit.app/"
 url5 = "https://blog-builder.streamlit.app/"
 url6 = "https://linkedin-enrich.streamlit.app/"
+url7 = "https://querychatbot.streamlit.app/"
 
 # Create hyperlinks using markdown
+st.header("Subhraneel")
 st.markdown(f"[Dynamic Prompt Generator🚀]({url1})")
 st.markdown(f"[Static Workflows🔄]({url2})")
 st.markdown(f"[Strategy Module♞]({url3})")
 st.markdown(f"[Brand Voice Prompt Generator🎙️]({url4})")
 st.markdown(f"[Blog Builder📝]({url5})")
 st.markdown(f"[Lead/Contact Enrichment from LinkedIn Profile ✨]({url6})")
+
+st.header("Subham")
+st.markdown(f"[ChatBot]({url7})")
